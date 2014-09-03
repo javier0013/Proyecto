6c687e01f52d04aba3623a3d05556e90c211122b
Javier Antonio Rosales Badillo <javierrosalesbadillo@gmail.com>
2014-09-03 10:57:45 -0500

    tercera

--- persona.java
+++ persona.java
@@ -1,7 +1,8 @@
 class persona{
 String nombre;
 String Apellido;
+
 	public static void main (String [] args){
-		Sytem.out.println("hola mundo en java");
+		Sytem.out.println("hola mundo en java 2.5");
 	}
 }
