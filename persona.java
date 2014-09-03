caad82bcd57fda0a4842ce6473c2c592b8aa4aba
Javier Antonio Rosales Badillo <javierrosalesbadillo@gmail.com>
2014-09-03 13:25:49 -0500

    se agrego una s en apellios

--- persona.java
+++ persona.java
@@ -1,7 +1,16 @@
 class persona{
 String nombre;
-String Apellido;
-
+String Apellidos;
+	public Persona(String nombre){
+		this.nombre=nombre;
+	}
+	public void agregarPersona(){
+		//codigo en desarrollo
+		//codigo en desarrollo
+		//codigo en desarrollo
+		//codigo en desarrollo
+		//codigo en desarrollo
+	}
 	public static void main (String [] args){
 		Sytem.out.println("hola mundo en java 2.5");
 	}
